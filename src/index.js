6803f308823bc93d6691e458df899f1263a3d42b
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-01 17:39:51 +0100

    handle event

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom'
 import Env from './env';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = Env.API_KEY;
 
 const App = () => {
-  return <div> {API_KEY}</div>;
+  return (
+    <div>
+      <SearchBar/>
+    </div>
+  );
 };
 
 ReactDom.render(<App/>, document.querySelector('.container'));
