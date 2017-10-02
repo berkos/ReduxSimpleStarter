cc2b5da18e43840ae2d2021834614247bb15caec
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-02 18:13:06 +0100

    Display the length of the videos in the video list

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import Env from './env';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = Env.API_KEY;
 
-YTSearch({key: API_KEY, term: 'psarogiorgis'}, function (data) {
-  console.log(data);
-});
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'psarogiorgis'}, (videos) => {
+      this.setState({videos: videos});
+    });
+  }
+
   render() {
     return (
       <div>
         <SearchBar/>
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
