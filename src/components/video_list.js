a9e0cb2d8dab9e33b5b7be4b3806895650c0f43f
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-06 09:20:19 +0100

    Create VideoList

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import VideolistItem from './video_list_item';
 
 const VideoList = (props) => {
-  const videos = props.videos;
+  const videoItems = props.videos.map((video => {
+    return <VideolistItem video={video}/>
+  }));
 
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   );
 };
