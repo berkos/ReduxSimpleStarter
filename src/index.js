ae551c4c5eb204cb85f8938516a8a68cccc043e9
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-06 12:29:07 +0100

    Fix bug, typo and make sure it renders the iframe

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 const API_KEY = Env.API_KEY;
 
@@ -23,6 +25,7 @@ class App extends React.Component {
     return (
       <div>
         <SearchBar/>
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     );
