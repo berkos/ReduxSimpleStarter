ff5aec11ecff0ec24b765fbfa86ad06ef12703b4
Antonis Berkakis <antonis.berkakis@simplybusiness.co.uk>
2017-10-08 21:47:01 +0100

    Add some styling

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -13,7 +13,7 @@ class SearchBar extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input onChange={event => this.setState({term: event.target.value})}/>
       </div>
     );
